a498b7f4aa64f68e0656f094e228b6e03e80cbce
Valentina <va.fernandezm@duocuc.cl>
2025-06-07 21:10:52 -0400

    nuevo proyecto

---
+++ src/main/java/com/perfulandia/soporteticket/SoporteticketApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandia.soporteticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoporteticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoporteticketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perfulandia/soporteticket/SoporteticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.soporteticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoporteticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
